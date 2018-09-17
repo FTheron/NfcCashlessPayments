102e2c57ee5e0bfcf39a4a11dba8497d1d17016c
Francois Theron <FrancoisTheron@CapitecBank.co.za>
2018-09-17 09:04:12 +0200

    Adding MoneyTextWatcher

---
+++ app/src/main/java/za/co/rsadevelopers/android/MoneyTextWatcher.java
@@ -0,0 +1,42 @@
+package za.co.rsadevelopers.android;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.lang.ref.WeakReference;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
+public class MoneyTextWatcher implements TextWatcher {
+    private final WeakReference<EditText> editTextWeakReference;
+
+    public MoneyTextWatcher(EditText editText) {
+        editTextWeakReference = new WeakReference<EditText>(editText);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        EditText editText = editTextWeakReference.get();
+        if (editText == null) return;
+        String s = editable.toString();
+        if (s.isEmpty()) return;
+        editText.removeTextChangedListener(this);
+        String cleanString = s.replaceAll("[R,.]", "");
+        BigDecimal parsed = new BigDecimal(cleanString).setScale(2, BigDecimal.ROUND_FLOOR).divide(new BigDecimal(100), BigDecimal.ROUND_FLOOR);
+        Locale locale = new Locale("en","ZA");
+        String formatted = NumberFormat.getCurrencyInstance(locale).format(parsed);
+        editText.setText(formatted);
+        editText.setSelection(formatted.length());
+        editText.addTextChangedListener(this);
+    }}
