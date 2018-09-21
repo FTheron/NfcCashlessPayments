3ca40e05f59104c7028a6b0e4d57a382e7510398
Francois Theron <ftheron@outlook.com>
2018-09-21 14:35:43 +0200

    Progress made on the payment activity.

--- app/src/main/java/za/co/rsadevelopers/android/MainActivity.java
+++ app/src/main/java/za/co/rsadevelopers/android/MainActivity.java
@@ -1,23 +1,21 @@
 package za.co.rsadevelopers.android;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button payButton;
+    EditText priceEditText;
+    TextView nfcStatusView;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,30 +23,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Add a listener to make sure that the format of the payment amount is correct.
-        EditText priceEditText = (EditText) findViewById(R.id.price_edit_text);
+        priceEditText = (EditText) findViewById(R.id.price_edit_text);
         priceEditText.addTextChangedListener(new MoneyTextWatcher(priceEditText));
 
         // Add event for paying.
-        Button mEmailSignInButton = (Button) findViewById(R.id.pay_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+        payButton = (Button) findViewById(R.id.pay_button);
+        payButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Pay();
+                clickPayButton();
             }
         });
+
+        nfcStatusView = (TextView) findViewById(R.id.nfc_status);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        CheckLogin();
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        checkLogin();
+        resetActivity();
     }
 
-    private void CheckLogin() {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        Long user_id = prefs.getLong("User_Id", 0);
-        String email = prefs.getString("User_Email", "none");
+    private void resetActivity(){
+        payButton.setText("Pay");
+        priceEditText.setText("");
+        nfcStatusView.setText("");
+        SharedPreferences.Editor prefs = sharedPreferences.edit();
+        prefs.putString("Payment_Amount","");
+        prefs.apply();
+    }
+
+    private void checkLogin() {
+        Long user_id = sharedPreferences.getLong("User_Id", 0);
+        String email = sharedPreferences.getString("User_Email", "none");
         if (user_id == 0) {
             // Redirect to login screen
             Intent intent = new Intent(this, LoginActivity.class);
@@ -60,12 +70,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public static final String PAYMENT_AMOUNT = "za.co.rsadevelopers.android.MESSAGE";
-    private void Pay() {
-        Intent intent = new Intent(this, PaymentActivity.class);
-        EditText editText = (EditText) findViewById(R.id.price_edit_text);
-        String message = editText.getText().toString();
-        intent.putExtra(PAYMENT_AMOUNT, message);
-        startActivity(intent);
+    private void clickPayButton() {
+        if (payButton.getText().equals("Pay")){
+            String amount = priceEditText.getText().toString();
+            SharedPreferences.Editor prefs = sharedPreferences.edit();
+            prefs.putString("Payment_Amount",amount);
+            prefs.apply();
+            payButton.setText("Cancel Payment");
+            nfcStatusView.setText("Waiting on NFC tag");
+        }else{
+            resetActivity();
+        }
     }
 }
--- app/src/main/java/za/co/rsadevelopers/android/PaymentActivity.java
+++ app/src/main/java/za/co/rsadevelopers/android/PaymentActivity.java
@@ -1,52 +1,67 @@
 package za.co.rsadevelopers.android;
 
 import android.content.Intent;
+import android.nfc.FormatException;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 
 public class PaymentActivity extends AppCompatActivity {
+    public static final String ERROR_DETECTED = "No NFC tag detected!";
+    public static final String WRITE_SUCCESS = "Text written to the NFC tag successfully!";
+    public static final String WRITE_ERROR = "Error during writing, is the NFC tag close enough to your device?";
+
+    ImageView nfcStatusImage;
+    TextView nfcStatusMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_payment);
 
         // Add event for paying.
-        Button cancelPaymentButton = (Button) findViewById(R.id.cancel_payment_button);
-        cancelPaymentButton.setOnClickListener(new View.OnClickListener() {
+        Button backButton = (Button) findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                CancelPayment();
+                completePayment();
             }
         });
+
+        nfcStatusImage = (ImageView) findViewById(R.id.nfc_payment_status_image);
+        backButton.setVisibility(View.GONE);
+
+        nfcStatusMessage = (TextView) findViewById(R.id.nfc_status_message);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.PAYMENT_AMOUNT);
-
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.payment_amount);
-        textView.setText(message);
+        setProgressBarIndeterminateVisibility(true);
     }
 
     // Catches the nfc scan event and processes the tag.
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
+        nfcStatusMessage.setText("1");
         if (intent != null && NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
             Parcelable[] rawMessages =
                     intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
@@ -56,19 +71,66 @@ public class PaymentActivity extends AppCompatActivity {
                     messages[i] = (NdefMessage) rawMessages[i];
                 }
                 // Process the messages array.
+                processTag(messages);
             }
         }
     }
 
-    protected void CreateTag(){
-        NdefRecord uriRecord = new NdefRecord(
-                NdefRecord.TNF_ABSOLUTE_URI ,
-                "android.rsadevelopers.co.za/wallet".getBytes(Charset.forName("US-ASCII")),
-                new byte[0], new byte[0]);
-        NdefRecord rtdUriRecord = NdefRecord.createUri("android.rsadevelopers.co.za/wallet");
+    private void processTag(NdefMessage[] msgs){
+        if (msgs == null || msgs.length == 0) return;
+        nfcStatusMessage.setText("2");
+        String text = "";
+        // String tagId = new String(msgs[0].getRecords()[0].getType());
+        byte[] payload = msgs[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16"; // Get the Text Encoding
+        int languageCodeLength = payload[0] & 0063; // Get the Language Code, e.g. "en"
+        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+
+        try {
+            // Get the Text
+            text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        nfcStatusMessage.setText("3");
+        nfcStatusMessage.setText(text);
+        setProgressBarIndeterminateVisibility(false);
     }
 
-    protected void CancelPayment(){
+    private void write(String text, Tag tag) throws IOException, FormatException {
+        NdefRecord[] records = { createRecord(text) };
+        NdefMessage message = new NdefMessage(records);
+        // Get an instance of Ndef for the tag.
+        Ndef ndef = Ndef.get(tag);
+        // Enable I/O
+        ndef.connect();
+        // Write the message
+        ndef.writeNdefMessage(message);
+        // Close the connection
+        ndef.close();
+    }
+
+    private NdefRecord createRecord(String text) throws UnsupportedEncodingException {
+        String lang       = "en";
+        byte[] textBytes  = text.getBytes();
+        byte[] langBytes  = lang.getBytes("US-ASCII");
+        int    langLength = langBytes.length;
+        int    textLength = textBytes.length;
+        byte[] payload    = new byte[1 + langLength + textLength];
+
+        // set status byte (see NDEF spec for actual bits)
+        payload[0] = (byte) langLength;
+
+        // copy langbytes and textbytes into payload
+        System.arraycopy(langBytes, 0, payload, 1,              langLength);
+        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
+
+        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN,  NdefRecord.RTD_TEXT,  new byte[0], payload);
+
+        return recordNFC;
+    }
+
+    private void completePayment(){
         finish();
     }
 
